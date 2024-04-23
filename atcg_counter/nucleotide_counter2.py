a82bb0760462b8b2bd38ad0ee8e96534e0445d3e
alondrayolotzin <alondram@lcg.unam.mx>
2024-04-22 21:01:28 -0600

    Se modifico para que marque como caracter no valido, cuando se ingrese algo que no corresponda a un nucleotido

--- atcg_counter/nucleotide_counter2.py
+++ atcg_counter/nucleotide_counter2.py
@@ -10,26 +10,25 @@ def count_nucleotides(sequence, nucleotides):
                     if nucleotide in counts:
                         counts[nucleotide] += 1
                         total += 1
+                    else:
+                        print(f"Invalid character: {nucleotide}")
+                        return None, None
     except FileNotFoundError:
         print("Sorry, couldn't find the file.")
         return None, None
     return counts, total
 
-def main():
-    parser = argparse.ArgumentParser(description='Count nucleotides in a DNA sequence.')
-    parser.add_argument('sequence', metavar='sequence', type=str, help='Path to the DNA sequence file')
-    parser.add_argument('-n', '--nucleotides', nargs='+', default=['A', 'T', 'G', 'C'], help='Nucleotides to count (default: all)')
-    args = parser.parse_args()
+parser = argparse.ArgumentParser(description='Count nucleotides in a DNA sequence.')
+parser.add_argument('sequence', metavar='sequence', type=str, help='Path to the DNA sequence file')
+parser.add_argument('-n', '--nucleotides', nargs='+', default=['A', 'T', 'G', 'C'], help='Nucleotides to count (default: all)')
+args = parser.parse_args()
 
-    counts, total = count_nucleotides(args.sequence, args.nucleotides)
+counts, total = count_nucleotides(args.sequence, args.nucleotides)
 
-    if counts is not None and total is not None and total != 0:  # Verifica si total es distinto de cero
-        print("Nucleotide counts:")
-        for nucleotide, count in counts.items():
-            print(f"{nucleotide}: {count} ({count/total*100:.2f}%)")
-    else:
-        print("No nucleotides found in the sequence.")
-
-if __name__ == "__main__":
-    main()
-
+if counts is not None and total is not None and total != 0:
+    print("Nucleotide counts:")
+    for nucleotide, count in counts.items():
+        print(f"{nucleotide}: {count}")
+else:
+    print("No nucleotides found in the sequence.")
+    
